3e639aecd18b5a2ab3c604a9ddacddf45aa5432d
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-10-19 15:07:41 +0200

    added wercker.json

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 
 describe "Decepticon API" do
-
   it "should respond to GET" do
     get '/decepticons.json'
     last_response.should be_ok
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require File.join(File.dirname(__FILE__), '..', 'main.rb')
 require 'sinatra'
 require 'rack/test'
 
-# setup test environment
 set :environment, :test
 set :run, false
 set :raise_errors, true
